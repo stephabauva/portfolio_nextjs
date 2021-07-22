7e363341a8454d2260da7ca5ec447d2e6fc10670
sb <stephane.baudry.vanaa@gmail.com>
2021-07-23 00:32:13 +0200

    adde Links and socialIcons to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
-import Link from "next/link";
-import React from "react";
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
-import { SiNextDotJs } from "react-icons/di";
+import Link from 'next/link';
+import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { RiDiscordLine } from 'react-icons/Ri';
+import { SiNextDotJs } from 'react-icons/si';
 
 import {
   Container,
@@ -10,17 +11,53 @@ import {
   Div3,
   NavLink,
   SocialIcons,
-} from "./HeaderStyles";
+  Span,
+} from './HeaderStyles';
 
 const Header = () => (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <SiNextDotJs size="3rem" /> <span>Portfolio</span>
+        <a
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            color: 'white',
+            marginBottom: '20px',
+          }}
+        >
+          <SiNextDotJs size="3rem" /> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/stephanBV">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/stephan-baudry/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://discordapp.com/users/567363754988404762">
+        <RiDiscordLine size="3rem" />
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
